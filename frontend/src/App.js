b521ab1b3cad5f2f52d0b406240948b3a4559ba1
MEGBLETO carnel <carnelilordwi@gmail.com>
2021-05-01 04:29:45 +0200

    Ui is starting to look like something

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import {Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <Header/>
       <main className='py-3'>
         <Container>
-        <p>Hello</p>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer />
